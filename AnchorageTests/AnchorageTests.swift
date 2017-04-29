d871c0e373b77281a520fbc580e28e1c35ac9ea3
Zev Eisenberg <zev.eisenberg@raizlabs.com>
2017-04-29 18:59:24 -0400

    Add better debug description for NSLayoutAttribute.

--- AnchorageTests/AnchorageTests.swift
+++ AnchorageTests/AnchorageTests.swift
@@ -343,3 +343,33 @@ extension AnchorageTests {
     }
 
 }
+
+extension NSLayoutAttribute: CustomDebugStringConvertible {
+
+    public var debugDescription: String {
+        switch self {
+        case .left: return "left"
+        case .right: return "right"
+        case .top: return "top"
+        case .bottom: return "bottom"
+        case .leading: return "leading"
+        case .trailing: return "trailing"
+        case .width: return "width"
+        case .height: return "height"
+        case .centerX: return "centerX"
+        case .centerY: return "centerY"
+        case .lastBaseline: return "lastBaseline"
+        case .firstBaseline: return "firstBaseline"
+        case .leftMargin: return "leftMargin"
+        case .rightMargin: return "rightMargin"
+        case .topMargin: return "topMargin"
+        case .bottomMargin: return "bottomMargin"
+        case .leadingMargin: return "leadingMargin"
+        case .trailingMargin: return "trailingMargin"
+        case .centerXWithinMargins: return "centerXWithinMargins"
+        case .centerYWithinMargins: return "centerYWithinMargins"
+        case .notAnAttribute: return "notAnAttribute"
+        }
+    }
+
+}
