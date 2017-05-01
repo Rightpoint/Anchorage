1e9433adea7a162749e76410364f6ed0b0f332e9
Zev Eisenberg <zev.eisenberg@raizlabs.com>
2017-05-01 17:55:12 -0400

    Delete unused testing function.

--- AnchorageTests/AnchorageTests.swift
+++ AnchorageTests/AnchorageTests.swift
@@ -423,15 +423,6 @@ extension AnchorageTests {
         XCTAssertTrue(expression1() === expression2(), message, file: file, line: line)
     }
 
-    struct UnexpectedNilError: Error {}
-    func assertNotNilAndUnwrap<T>(_ variable: T?, message: String = "Unexpected nil variable", file: StaticString = #file, line: UInt = #line) throws -> T {
-        guard let variable = variable else {
-            XCTFail(message, file: file, line: line)
-            throw UnexpectedNilError()
-        }
-        return variable
-    }
-
 }
 
 public extension BinaryFloatingPoint {
