5dc1b6734cc72aad74e5a53d6e3ed8124c239150
Zev Eisenberg <zev.eisenberg@raizlabs.com>
2017-06-09 10:21:26 -0400

    Build both Swift 3 and Swift 4. Run on iOS 11 and tvOS 11 simulators.

--- scripts/iOS.sh
+++ scripts/iOS.sh
@@ -5,7 +5,19 @@ set -o pipefail && \
   -project Anchorage.xcodeproj \
   -scheme Anchorage-iOS \
   -sdk iphonesimulator \
-  -destination "platform=iOS Simulator,name=iPhone 6S,OS=10.3" \
+  -destination "platform=iOS Simulator,name=iPhone 6S,OS=11.0" \
+  SWIFT_VERSION=3.0 \
+  CODE_SIGNING_REQUIRED=NO \
+  CODE_SIGN_IDENTITY= \
+  | xcpretty
+
+set -o pipefail && \
+  xcodebuild clean build test \
+  -project Anchorage.xcodeproj \
+  -scheme Anchorage-iOS \
+  -sdk iphonesimulator \
+  -destination "platform=iOS Simulator,name=iPhone 6S,OS=11.0" \
+  SWIFT_VERSION=4.0 \
   CODE_SIGNING_REQUIRED=NO \
   CODE_SIGN_IDENTITY= \
   | xcpretty
--- scripts/tvOS.sh
+++ scripts/tvOS.sh
@@ -5,7 +5,19 @@ set -o pipefail && \
   -project Anchorage.xcodeproj \
   -scheme Anchorage-tvOS \
   -sdk appletvsimulator \
-  -destination "platform=tvOS Simulator,name=Apple TV 1080p,OS=10.2" \
+  -destination "platform=tvOS Simulator,name=Apple TV 1080p,OS=11.0" \
+  SWIFT_VERSION=3.0 \
+  CODE_SIGNING_REQUIRED=NO \
+  CODE_SIGN_IDENTITY= \
+  | xcpretty
+
+set -o pipefail && \
+  xcodebuild clean build test \
+  -project Anchorage.xcodeproj \
+  -scheme Anchorage-tvOS \
+  -sdk appletvsimulator \
+  -destination "platform=tvOS Simulator,name=Apple TV 1080p,OS=11.0" \
+  SWIFT_VERSION=4.0 \
   CODE_SIGNING_REQUIRED=NO \
   CODE_SIGN_IDENTITY= \
   | xcpretty
