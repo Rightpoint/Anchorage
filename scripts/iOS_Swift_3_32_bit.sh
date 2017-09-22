9301e3fb1b5ce58e2fe258242be6966f93895737
Zev Eisenberg <zev.eisenberg@raizlabs.com>
2017-09-22 03:07:37 -0400

    Match iOS versions from CircleCI.

--- scripts/iOS_Swift_3_32_bit.sh
+++ scripts/iOS_Swift_3_32_bit.sh
@@ -5,7 +5,7 @@ set -o pipefail && \
   -project Anchorage.xcodeproj \
   -scheme Anchorage-iOS \
   -sdk iphonesimulator \
-  -destination "platform=iOS Simulator,name=iPhone 5,OS=9.0" \
+  -destination "platform=iOS Simulator,name=iPhone 5,OS=10.3.1" \
   SWIFT_VERSION=3.0 \
   CODE_SIGNING_REQUIRED=NO \
   CODE_SIGN_IDENTITY= \
