972fe7b1dfa7735d65b72041101fd450607540c5
Zev Eisenberg <zev.eisenberg@raizlabs.com>
2017-09-22 08:23:22 -0400

    Match tvOS versions from CircleCI.

--- scripts/tvOS_Swift_4.sh
+++ scripts/tvOS_Swift_4.sh
@@ -5,7 +5,7 @@ set -o pipefail && \
   -project Anchorage.xcodeproj \
   -scheme Anchorage-tvOS \
   -sdk appletvsimulator \
-  -destination "platform=tvOS Simulator,name=Apple TV 1080p,OS=11.0" \
+  -destination "platform=tvOS Simulator,name=Apple TV,OS=11.0" \
   SWIFT_VERSION=4.0 \
   CODE_SIGNING_REQUIRED=NO \
   CODE_SIGN_IDENTITY= \
