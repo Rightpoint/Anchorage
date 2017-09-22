dc37bd3d79dac5d7ca562898ab2f889bf1f52d13
Zev Eisenberg <zev.eisenberg@raizlabs.com>
2017-09-22 02:48:00 -0400

    Use iPhone 5 on CircleCI, because iPhone 4S is not available.

--- scripts/iOS_Swift_3_32_bit.sh
+++ scripts/iOS_Swift_3_32_bit.sh
@@ -5,7 +5,7 @@ set -o pipefail && \
   -project Anchorage.xcodeproj \
   -scheme Anchorage-iOS \
   -sdk iphonesimulator \
-  -destination "platform=iOS Simulator,name=iPhone 4S,OS=9.0" \
+  -destination "platform=iOS Simulator,name=iPhone 5,OS=9.0" \
   SWIFT_VERSION=3.0 \
   CODE_SIGNING_REQUIRED=NO \
   CODE_SIGN_IDENTITY= \
