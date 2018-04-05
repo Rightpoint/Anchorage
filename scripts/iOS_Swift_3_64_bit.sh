d6cdb680bbf800e6bb41a70a498a3ad60f2abd6b
Zev Eisenberg <zev.eisenberg@raizlabs.com>
2018-04-05 18:33:01 -0400

    Build on CircleCI 2.0 and Xcode 9.3.

--- scripts/iOS_Swift_3_64_bit.sh
+++ scripts/iOS_Swift_3_64_bit.sh
@@ -5,7 +5,7 @@ set -o pipefail && \
   -project Anchorage.xcodeproj \
   -scheme Anchorage-iOS \
   -sdk iphonesimulator \
-  -destination "platform=iOS Simulator,name=iPhone 6s,OS=11.0.1" \
+  -destination "platform=iOS Simulator,name=iPhone 6s,OS=11.3" \
   SWIFT_VERSION=3.0 \
   CODE_SIGNING_REQUIRED=NO \
   CODE_SIGN_IDENTITY= \
