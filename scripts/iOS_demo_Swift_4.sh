f4792cda6d48451707b27e5d4a1f947820272f8b
Zev Eisenberg <zeisenberg@rightpoint.com>
2019-05-19 10:52:16 -0400

    Vary iOS version in demo script.

--- scripts/iOS_demo_Swift_4.sh
+++ scripts/iOS_demo_Swift_4.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
+# required: pass iOS version as first and only parameter
 
 set -o pipefail && \
   xcodebuild clean build \
   -project Anchorage.xcodeproj \
   -scheme AnchorageDemo \
   -sdk iphonesimulator \
-  -destination "platform=iOS Simulator,name=iPhone 6s,OS=12.1" \
+  -destination "platform=iOS Simulator,name=iPhone 6s,OS=$1" \
   SWIFT_VERSION=4.2 \
   CODE_SIGNING_REQUIRED=NO \
   CODE_SIGN_IDENTITY= \
