ca62b64c8a2fda0d181778bc2e486b9f5460bb23
Zev Eisenberg <zeisenberg@rightpoint.com>
2019-05-19 11:29:41 -0400

    Pass tvOS version for Swift 4 builds.

--- scripts/tvOS_Swift_4.sh
+++ scripts/tvOS_Swift_4.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
+# required: pass tvOS version as first and only parameter
 
 set -o pipefail && \
   xcodebuild clean build test \
   -project Anchorage.xcodeproj \
   -scheme Anchorage-tvOS \
   -sdk appletvsimulator \
-  -destination "platform=tvOS Simulator,name=Apple TV,OS=12.1" \
+  -destination "platform=tvOS Simulator,name=Apple TV,OS=$1" \
   SWIFT_VERSION=4.2 \
   CODE_SIGNING_REQUIRED=NO \
   CODE_SIGN_IDENTITY= \
